04ded97d4508c3e893cff490442295ca3f8cef35
gh0stp4wz <gh0st13@gh0stp4wz.is-a.dev>
2026-08-11 15:11:13 +0200

    Switch to BD-SCSS as preprocessor

---
+++ bd-scss.config.js
@@ -0,0 +1,10 @@
+/** @type {import('bd-scss/lib/config').Config} */
+export default {
+	meta: {
+		name: 'FluxerOverhaul',
+		author: 'Nexus-Themes',
+		version: '1.0.0',
+		description: 'An overhaul of the Fluxer UI',
+		source: 'https://github.com/Nexus-Themes/FluxerOverhaul'
+	}
+};
